e183bbd7e66911be057d0dde661c382ee96ad756
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-24 12:40:48 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Text } from 'react-native'
+
+const App:React.FC = () =>{
+  return(
+    <>
+      <Text> Spotify Clone </Text>
+    </>
+
+  )
+}
+
+export default App;
