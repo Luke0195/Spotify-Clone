bf1b217638bf9dceaef0b0777c32c274c0d3b256
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-25 18:08:54 -0300

    feat: add Album component

---
+++ src/components/Album/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native'
+
+const Album:React.FC  = () =>{
+  return(
+    <>
+      <Text> Hello Album</Text>
+    </>
+  )
+}
--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,24 +1,12 @@
-import React from  'react';
-import SongSuggestions from '../../components/SongsSuggestions'
-import {Container, Header, HeaderTitle,HeaderTopContent} from './styles';
-
-import EvilIcons from 'react-native-vector-icons/EvilIcons';
-
-
-const Feed:React.FC  =() =>{
+import React from 'react';
+import {Text} from 'react-native'
+export const Feed:React.FC  = ()  =>{
   return(
-  <>
-      <Container>
-        <Header>
-           <HeaderTopContent>
-             <HeaderTitle> Bom dia </HeaderTitle>
-              <EvilIcons name="gear" size={32} color="#f5f5f5"/>
-           </HeaderTopContent>
-           <SongSuggestions/>
-        </Header>
-      </Container>
-  </>
+    <>
+      <Text> Spotify Clone</Text>
+    </>
   )
 }
+
 
 export default Feed;
